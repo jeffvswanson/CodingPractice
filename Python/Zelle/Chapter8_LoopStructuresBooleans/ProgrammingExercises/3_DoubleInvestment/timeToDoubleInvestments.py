64599f113020cee0a792e7254225ca4817bc132a
jeffvswanson <jeff.v.swanson@gmail.com>
2018-08-04 20:26:14 -0600

    Repository alignment

--- Python/Zelle/Chapter8_LoopStructuresBooleans/ProgrammingExercises/3_DoubleInvestment/timeToDoubleInvestments.py
+++ Python/Zelle/Chapter8_LoopStructuresBooleans/ProgrammingExercises/3_DoubleInvestment/timeToDoubleInvestments.py
@@ -7,16 +7,7 @@ interest rate, and the output is the number of years it takes an investment to
 double. Note: The amount of the initial investment does not matter; you can
 use $1."""
 
-<<<<<<< HEAD
 def getAPR():
-=======
-def main():
-
-	print("This program calculates how long it takes for an investment to \
-double at a given interest rate in years.")
-	initPrincipal = 1
-	principal = initPrincipal
->>>>>>> 9df6db465f2855ee3d149aa825ffa491706f03a7
 	try:
 		apr = float(input("Please enter the interest rate as a decimal: "))
 	except (SyntaxError, NameError, TypeError, ValueError) as err:
@@ -27,19 +18,9 @@ double at a given interest rate in years.")
 		print("Investments can't double if they're not greater than zero. \
 Exiting.")
 		quit(0)
-<<<<<<< HEAD
 	return apr
 
 def result(apr, years):
-=======
-	
-	# Initialize the number of years to zero.
-	years = 0
-	while principal <= (initPrincipal * 2):
-		principal = principal + (principal * apr)
-		years = years + 1
-	
->>>>>>> 9df6db465f2855ee3d149aa825ffa491706f03a7
 	if years < 1:
 		print("The amount of time it will take to double the initial \
 investment at {0:.1%} APR is {1} of a year.".format(apr, years))
@@ -49,7 +30,6 @@ investment as {0:.1%} APR is 1 year.".format(apr))
 	else:
 		print("The amount of time it will take to double at a {0:.1%} APR is {1} \
 years.".format(apr, years))
-<<<<<<< HEAD
 
 def main():
 
@@ -67,8 +47,4 @@ double at a given interest rate in years.")
 	
 	# Print a grammatically correct sentence for year number.
 	result(apr, years)
-=======
-		
-
->>>>>>> 9df6db465f2855ee3d149aa825ffa491706f03a7
 main()
